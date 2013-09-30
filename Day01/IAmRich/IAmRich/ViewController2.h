90260a5a30417f088fe8c33a84abbf460dc9bc8f
thanhbin <thanhbin15051989@gmail.com>
2013-09-30 20:51:22 +0700

    thanhbin bo xung viewcontroller2

---
+++ Day01/IAmRich/IAmRich/ViewController2.h
@@ -0,0 +1,13 @@
+//
+//  ViewController2.h
+//  IAmRich
+//
+//  Created by iOS18 on 9/30/13.
+//  Copyright (c) 2013 Techmaster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController2 : UIViewController
+
+@end
